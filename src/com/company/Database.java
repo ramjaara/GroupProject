113e384332122e513066995a9dfb669882075c5c
Ramjaar <bekasramjaar@gmail.com>
2022-03-20 22:36:54 +0000

    Began the database template to access at school

---
+++ src/com/company/Database.java
@@ -0,0 +1,22 @@
+package com.company;
+
+//IGNORE THIS ALL, im trying to make the data base work but it didnt so im redoing it
+
+public class Database {
+
+}
+
+//package databases;
+
+//import java.sql.*;
+//public class Databases {
+
+   // public static void main(String[] args) {
+       // GUITable gt = new GUITable();
+       // gt.setVisible(true);
+
+//        GUISingle gs = new GUISingle();
+//        gs.setVisible(true);
+   // }
+
+//}
